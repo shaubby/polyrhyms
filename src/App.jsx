fecc0682f4b7bf248306d1c462875701a8669abd
Rishabh Agarwal <agar5333@gmail.com>
2025-05-06 17:13:08 -0500

    create

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import Beat from './Beat';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1 >Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <Beat/>
+      {/* <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/Beat.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function Beat() {
+
+    const [isPlaying, setIsPlaying] = useState(0);
+
+
+    return (
+        <>
+        <div className="flex bg-gray-100 ">
+            <p>woiejf</p>
+        </div>
+        </>
+    )
+}
+
+export default Beat
