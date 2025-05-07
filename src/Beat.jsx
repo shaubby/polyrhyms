d3f7e50bf939d601c730ed56f19ea5b782c1a084
Rishabh Agarwal <agar5333@gmail.com>
2025-05-07 10:11:50 -0500

    yay

--- src/Beat.jsx
+++ src/Beat.jsx
@@ -2,15 +2,23 @@ import { useState } from 'react'
 
 function Beat() {
 
-    const [isPlaying, setIsPlaying] = useState(0);
-
+    const [isPlaying, setIsPlaying] = useState(true);
+    const handleClick = () => {
+        setIsPlaying(false);
+    }
 
     return (
-        <>
-        <div className="flex bg-gray-100 ">
-            <p>woiejf</p>
+        <div className="flex">
+        <div className="flex-1 h-12 bg-red-50" onClick={handleClick}>
+            <p>1</p>
         </div>
-        </>
+        <div className="flex-1 h-12 bg-red-100" onClick={handleClick}>
+            <p>1</p>
+        </div>
+        <div className="flex-1 h-12 bg-red-500" onClick={handleClick}>
+            <p>1</p>
+        </div>
+        </div>
     )
 }
 
