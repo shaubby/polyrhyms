f845ef658cb1f12946ad72488c4bbce1a8402984
shaubby <agar5333@gmail.com>
2025-05-19 18:58:23 -0500

    5/19

--- src/Visualizer.jsx
+++ src/Visualizer.jsx
@@ -5,13 +5,14 @@ function Row(props) {
 
     
     return (
-        <div className='p-4 mb-6 bg-black flex rounded-3xl'>
-            <div className='flex-auto'> 
-            {props.beat1.map((button, index) => ( <div className='flex-1'><div key={index} className={"relative w-2 h-10 rounded-full bg-red-500"}></div></div>))}
+        <div className='p-6 mb-6 bg-black rounded-3xl'>
+            <div className='flex mb-2'> 
+            {props.beat1.map((button, index) => ( <div className='flex-1'><div key={index} className={"relative w-2 h-10 rounded-full " + (button==0 ? "bg-red-500" : "bg-black")}></div></div>))}
             </div>
-            <div className='flex-auto' >
-            {props.beat2.map((button, index) => ( <div className='flex-1'><div key={index} className={"relative w-2 h-10 rounded-full bg-blue-500"}></div></div>))}
-            </div>
+             <div className='flex'>
+            {props.beat2.map((button, index) => ( <div className='flex-1'><div key={index} className={"relative w-2 h-10 rounded-full " + (button==0 ? "bg-blue-500" : "bg-black")}></div></div>))}
+            </div> 
+            <div className=''></div>
         </div>
 
     )
