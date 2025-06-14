992ef8a33702cf4b4d3b975a81e7877aa2e9c191
shaubby <agar5333@gmail.com>
2025-06-14 15:17:03 -0500

    fix await?

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  esbuild: {
+    supported: {
+      'top-level-await': true
+    },
+  },
   plugins: [react(), tailwindcss()],
-  base: "/polyrhyms"
+  base: "/polyrhyms/"
 })
