631e3bd7f83b56dd3ddbd1417c90331f9cf4a06a
shaubby <agar5333@gmail.com>
2025-06-14 18:02:16 -0500

    quick fix

--- src/Popup.jsx
+++ src/Popup.jsx
@@ -11,12 +11,12 @@ function Popup() {
     }
     return (
        
-        <div className='absolute right-6 bottom-6 sm:top-6'>
+        <div className='absolute right-6 bottom-6'>
              {visible? <div>
             <div onClick={handleClick} className={"w-10 h-10 absolute right-0 bg-white border-3 border-black font-mono text-black rounded-3xl flex justify-center items-center text-2xl select-none"}>x</div>
             <div className={"w-60 m-4 bg-white border-3 border-black font-mono text-black rounded-3xl px-5 py-5 "}>
                  Make sure to turn off silent/vibrate!<br/>
-                Go star on <a className = "underline" href="https://github.com/shaubby/polyrhyms" target="_blank">github</a>!!
+                <p className='font-bold'>Please go star on <a className = "underline" href="https://github.com/shaubby/polyrhyms" target="_blank">github</a>!!</p>
                 
             </div>
             </div> : null}
