4de0227398699923b4fc88cf0abdca1d87a3ecfa
shaubby <agar5333@gmail.com>
2025-06-14 17:10:46 -0500

    please work ts not deploying

--- src/Popup.jsx
+++ src/Popup.jsx
@@ -1,19 +1,10 @@
 import { useState, useEffect } from 'react'
-const response = await fetch("https://api.github.com/repos/shaubby/polyrhyms", {
-    method: "GET",
-    headers: {
-      Authorization: `token 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 ` 
-    }});
-const json = await response.json();
+
 function Popup() {
 
     
     const [visible, setVisible] = useState(true);
-    const [stars, setStars] = useState(0);
-    useEffect(() => { 
 
-        setStars(json.stargazers_count);  
-    }, []);
 
     const handleClick = () => {
         setVisible(false)
@@ -24,9 +15,9 @@ function Popup() {
              {visible? <div>
             <div onClick={handleClick} className={"w-10 h-10 absolute right-0 bg-white border-3 border-black font-mono text-black rounded-3xl flex justify-center items-center text-2xl select-none"}>x</div>
             <div className={"w-60 m-4 bg-white border-3 border-black font-mono text-black rounded-3xl px-5 py-5 "}>
+                 Make sure to turn off silent/vibrate!<br/>
                 Go star on <a className = "underline" href="https://github.com/shaubby/polyrhyms" target="_blank">github</a>!!
-                <br/>
-                {stars + '/150 stars' }
+                
             </div>
             </div> : null}
             
