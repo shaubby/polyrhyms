4f01c3dd5b96906fb13c410a801eaf178d498dae
shaubby <agar5333@gmail.com>
2025-06-14 15:26:47 -0500

    still trying to deploy

--- src/Popup.jsx
+++ src/Popup.jsx
@@ -1,20 +1,11 @@
 import { useState, useEffect } from 'react'
-const response = await fetch("https://api.github.com/repos/shaubby/polyrhyms", {
-    method: "GET",
-    headers: {
-      Authorization: `token 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` 
-    }});
-const json = await response.json();
-console.log(json)
+
 function Popup() {
 
     
     const [visible, setVisible] = useState(true);
-    const [stars, setStars] = useState(0);
-    useEffect(() => { 
 
-        setStars(json.stargazers_count);  
-    }, []);
+
     const handleClick = () => {
         setVisible(false)
     }
@@ -25,8 +16,6 @@ function Popup() {
             <div onClick={handleClick} className={"w-10 h-10 absolute right-0 bg-white border-3 border-black font-mono text-black rounded-3xl flex justify-center items-center text-2xl select-none"}>x</div>
             <div className={"w-60 m-4 bg-white border-3 border-black font-mono text-black rounded-3xl p-5 "}>
                 Go star on <a className = "underline" href="https://github.com/shaubby/polyrhyms" target="_blank">github</a>!!
-                <br/>
-                {stars + '/150 stars' }
             </div>
             </div> : null}
             
--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  esbuild: {
-    supported: {
-      'top-level-await': true
-    },
-  },
   plugins: [react(), tailwindcss()],
-  base: "/polyrhyms/"
+  base: "/polyrhyms/",
 })
